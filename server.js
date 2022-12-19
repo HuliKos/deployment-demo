0ab677047aff224b429e5b94fd3f83e2b90e333c
Lan Kostrikin <lankostrikin@gmail.com>
2022-12-19 14:46:13 -0600

    test2

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path')
 app.use(cors())
 app.use(express.static("public"))
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')))
 
 app.listen(4000, 
     () => console.log('server running on 4000')
